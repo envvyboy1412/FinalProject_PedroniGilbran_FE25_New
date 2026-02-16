cdea7972d12ee9b6761d174f839045a96c3f7b00
Pedroni Gilbran <pedronibt45@gmail.com>
2026-02-16 12:36:49 +0700

    Persiapan

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,9 +2,27 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
+    <Html lang="id">
+      <Head>
+        {/* Bad Script */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Bad+Script&display=swap"
+          rel="stylesheet"
+        />
+
+        {/* Amarante */}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Amarante&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
